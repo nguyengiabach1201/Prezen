aa739fd48cfe40e90badbf9a74435910238322f6
nguyengiabach1201 <nguyengiabach1201@gmail.com>
2024-04-06 21:44:58 +0700

    Test

--- src/prezen-chart.js
+++ src/prezen-chart.js
@@ -92,13 +92,14 @@ let bars = document.querySelectorAll('.chart.bar');
 bars.forEach(chart => {
     console.log(chart);
     createChart(chart, 'bar');
-    console.log(chart);
 });
 let pies = document.querySelectorAll('.chart.pie');
 pies.forEach(chart => {
+    console.log(chart);
     createChart(chart, 'pie');
 });
 let lines = document.querySelectorAll('.chart.line');
 lines.forEach(chart => {
+    console.log(chart);
     createChart(chart, 'line');
 });
