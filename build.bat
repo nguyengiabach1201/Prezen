b07710c2de84d95c2fa2af3a81706a679fd2847d
Nguyễn Gia Bách <nguyengiabach1201@gmail.com>
2024-07-28 10:51:41 +0700

    Add files via upload

---
+++ build.bat
@@ -0,0 +1,19 @@
+@echo off
+setlocal enabledelayedexpansion
+@REM ,.\src\prezen.js
+set "file_list=.\plugins\tex-chtml.js,.\plugins\chart.js@2.8.0,.\src\js\prezen-chart.js,.\src\js\prezen.min.js" 
+set "output_file=prezen.js"
+
+set "output_path=."
+
+if not exist "%output_path%" mkdir "%output_path%"
+
+set output_file_path="%output_path%\%output_file%"
+
+type nul > "%output_file_path%"
+
+for %%a in ("%file_list%") do (
+    type "%%a" >> "%output_file_path%"
+)
+
+echo Merging complete. Output file: %output_file_path%
