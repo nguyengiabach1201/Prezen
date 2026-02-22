361f09e0b4a6b7701b3d7ab460fc1c01289ca5ea
Nguyễn Gia Bách <nguyengiabach1201@gmail.com>
2026-02-22 22:11:23 +0700

    Delete test.js

--- src/test.js
+++
@@ -1,8 +0,0 @@
-import { BrowserFinder } from '@agent-infra/browser-finder';
-
-const finder = new BrowserFinder();
-
-// Find any available browser (tries Chrome -> Edge -> Firefox)
-const browser = finder.findBrowser();
-console.log(browser.path); // "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
-console.log(browser.type); // "chrome"
